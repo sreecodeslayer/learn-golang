feb767c609c3095f07f4d60f310cb815897be479
Sreenadh TC <kesav.tc8@gmail.com>
2018-12-12 14:00:12 +0530

    declarations of varibales, expressions and assignments in golang

--- 3-declarations.go
+++ 3-declarations.go
@@ -1,5 +1,15 @@
 package main
+import "fmt"
 
 func main() {
-	
+	// declare and assign
+	x := 42
+	fmt.Println(x)
+	// reassign a declared variable
+	x = 29
+	fmt.Println(x)
+
+	// Statements with expression
+	y := 100+56+6
+	fmt.Println(y)
 }
